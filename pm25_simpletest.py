33c1e7ba1fca42258b3a30f5dcdc9ecb1b2bb03f
duncanvh <duncan.vh8@berkeley.edu>
2025-02-14 15:32:52 -0800

    Update pm25_simpletest.py

--- pm25_simpletest.py
+++ pm25_simpletest.py
@@ -86,9 +86,9 @@ while T <= 30:
     print("---------------------------------------")
     print()
 
-    print(time.localtime())
+    #print(time.localtime())
 
-    writer.writerow([["time","PM1","PM2.5", "PM10"]])
+    writer.writerow([current_time, aqdata['pm10 standard'],aqdata["pm25 standard"], aqdata["pm100 standard"]])
 
     T += 1
     time.sleep(1)

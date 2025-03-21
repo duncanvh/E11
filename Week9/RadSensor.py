505b03831d2b1fe544876d68d3cb8cd89e627717
duncanvh <duncan.vh8@berkeley.edu>
2025-03-21 15:34:31 -0700

    Update RadSensor.py

--- Week9/RadSensor.py
+++ Week9/RadSensor.py
@@ -12,9 +12,10 @@ try:
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
     GPIO.add_event_detect(17, GPIO.BOTH, callback=my_callback)
+    message = raw_input('\nPress any key to exit.\n')
 
-answer = my_callback(17)
-print(answer)
+#answer = my_callback(17)
+#print(answer)
 
 finally:
     GPIO.cleanup()

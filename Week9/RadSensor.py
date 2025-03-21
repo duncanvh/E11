269e8e0dc93ef466f857b0e1bcb4b99c692e8d5e
duncanvh <duncan.vh8@berkeley.edu>
2025-03-21 15:52:55 -0700

    Update RadSensor.py

--- Week9/RadSensor.py
+++ Week9/RadSensor.py
@@ -6,13 +6,12 @@ import time
 T = 0
 
 def my_callback(channel):
-
  print('\n▼  at ' + str(datetime.datetime.now()))
  print('\n ▲ at ' + str(datetime.datetime.now()))
  
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.add_event_detect(11, GPIO.BOTH, callback=my_callback)
+GPIO.add_event_detect(11, GPIO.FALLING, callback=my_callback)
 
 runtime = int(sys.argv[1])
 while T < runtime:
